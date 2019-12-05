56fad6bfa62a3982cda367f859d2e57df91811a4
SerhiiSenyk <email>
2019-12-05 15:17:09 +0200

    added IndexedDB

--- js/fans_appeal.js
+++ js/fans_appeal.js
@@ -1,102 +1,86 @@
 
-var localStorage = window.localStorage;
-const key = "fans_appeal";
 
-window.addEventListener("online",
-    function () {
-        if (isOnline() && localStorage.length > 0) {
-            showAllAppealsFromLocalStorage();
+const key = "fans_appeal";
+document.addEventListener("DOMContentLoaded", function () {
+    var fansAppeals = [];
+    window.addEventListener("online", function (event) {
+        storage.storage.get(key, (appeals) => {
+            if (appeals) {
+                fansAppeals = appeals;
+                fansAppeals.forEach(function (appeal) {
+                    showAppealOnPage(appeal.text, appeal.date)
+                });
+            }
+            storage.storage.delete(key);
             sendToServer();
+            fansAppeals = [];
+        });
+    });
+
+    if (isOnline()) {
+        fansAppeals.forEach(function (appeal) {
+            showAppealOnPage(appeal.text, appeal.date)
+        });
+        storage.storage.delete(key);
+        //sendToServer();
+        fansAppeals = [];
+    }
+
+    storage.storage.get(key, (appeals) => {
+        if (appeals) {
+            fansAppeals = appeals;
         }
     });
 
-if (isOnline() && localStorage.length > 0) {
-    showAllAppealsFromLocalStorage();
-}
-
-function isOnline() {
-    return window.navigator.onLine;
-}
-
-function validateFansAppeal(text) {
-    return (text === "") ? false : true;
-}
-
-function addFansAppeal() {
-    var textAppeal = document.getElementById("comment");
-    var text = textAppeal.value.trim();
-    var date = getCurrentDate();
-    var fansAppeals = JSON.parse(localStorage.getItem(key));
-    if (fansAppeals == null) {
-        fansAppeals = [];
+    function validateFansAppeal(text) {
+        return (text === "") ? false : true;
     }
-    if (!validateFansAppeal(text)) {
-        alert("Write an appeal!");
-        stop();
-    } else {
-        if (isOnline()) {
-            showAppealOnPage(text, date);
-            //sendToServer();
+
+    document.getElementById('addFansAppeal').addEventListener('click', function () {
+        var textAppeal = document.getElementById("comment");
+        var text = textAppeal.value.trim();
+        var date = getCurrentDate();
+        if (!validateFansAppeal(text)) {
+            alert("Write an appeal!");
+            stop();
         } else {
-            fansAppeals.push({ text: text, date: date });
-            addAppealToLocalStorage(fansAppeals);
-            alert("Added to local storage");
-        }
-    }
-    textAppeal.value = "";
-}
-
-function showAppealOnPage(appealText, dateTime) {
-    var appeals = document.getElementById('appeals');
-    var containerBlock = document.createElement("div");
-    var rowBlock = document.createElement("div");
-    var cardAppealBlock = document.createElement("div");
-    var cardInfoBlock = document.createElement("div");
-    containerBlock.className = "container";
-    rowBlock.className = "row";
-    cardAppealBlock.className = "card col-8";
-    cardInfoBlock.className = "card";
-    cardInfoBlock.innerHTML = "<p>" + "Serhii" + "<br>" + dateTime + "<p>";
-    cardAppealBlock.appendChild(document.createTextNode(appealText.trim()));
-    rowBlock.appendChild(cardInfoBlock);
-    rowBlock.appendChild(cardAppealBlock);
-    containerBlock.appendChild(rowBlock);
-    appeals.appendChild(document.createElement('hr'));
-    appeals.appendChild(containerBlock);
-}
-
-function addAppealToLocalStorage(appeals) {
-    const appealsJSON = JSON.stringify(appeals);
-    try {
-        localStorage.setItem(key, appealsJSON);
-    } catch (exeption) {
-        if (exeption == 'QUOTA_EXCEEDED_ERR') {
-            alert("Local storage is overflowed");
-        }
-    }
-}
-
-function showAllAppealsFromLocalStorage() {
-    const fansAppeals = JSON.parse(localStorage.getItem(key));
-    localStorage.removeItem(key);
-    if (fansAppeals != null) {
-        fansAppeals.forEach(function (appeal) {
-            if (appeal != null) {
-                showAppealOnPage(appeal.text, appeal.date);
+            if (isOnline()) {
+                showAppealOnPage(text, date);
+                //sendToServer();
+            } else {
+                fansAppeals.push({ text: text, date: date });
+                storage.storage.add(key, fansAppeals);
+                alert("Added to storage");
             }
-        });
+        }
+        textAppeal.value = "";
+    });
+
+    function showAppealOnPage(appealText, dateTime) {
+        var appeals = document.getElementById('appeals');
+        var containerBlock = document.createElement("div");
+        var rowBlock = document.createElement("div");
+        var cardAppealBlock = document.createElement("div");
+        var cardInfoBlock = document.createElement("div");
+        containerBlock.className = "container";
+        rowBlock.className = "row";
+        cardAppealBlock.className = "card col-8";
+        cardInfoBlock.className = "card";
+        cardInfoBlock.innerHTML = "<p>" + "Serhii" + "<br>" + dateTime + "<p>";
+        cardAppealBlock.appendChild(document.createTextNode(appealText.trim()));
+        rowBlock.appendChild(cardInfoBlock);
+        rowBlock.appendChild(cardAppealBlock);
+        containerBlock.appendChild(rowBlock);
+        appeals.appendChild(document.createElement('hr'));
+        appeals.appendChild(containerBlock);
     }
-}
 
-function sendToServer() {
-    alert("Sent to server");
-}
+    function getCurrentDate() {
+        const dateTime = new Date();
+        return String((dateTime.getHours() < 10 ? "0" : "") + dateTime.getHours() + ":" +
+            (dateTime.getMinutes() < 10 ? "0" : "") + dateTime.getMinutes() + ":" +
+            (dateTime.getSeconds() < 10 ? "0" : "") + dateTime.getSeconds() + "<br>" +
+            dateTime.getDate() + "." + (dateTime.getMonth() + 1) + "." + dateTime.getFullYear());
+    }
 
-
-function getCurrentDate() {
-    const dateTime = new Date();
-    return String((dateTime.getHours() < 10 ? "0" : "") + dateTime.getHours() + ":" +
-        (dateTime.getMinutes() < 10 ? "0" : "") + dateTime.getMinutes() + ":" +
-        (dateTime.getSeconds() < 10 ? "0" : "") + dateTime.getSeconds() + "<br>" +
-        dateTime.getDate() + "." + (dateTime.getMonth() + 1) + "." + dateTime.getFullYear());
-}
+});
--- js/news.js
+++ js/news.js
@@ -1,63 +1,68 @@
 const key = "news";
 
-var localStorage = window.localStorage;
+//var localStorage = window.localStorage;
+document.addEventListener("DOMContentLoaded", function () {
+    var all_news = [];
+    window.addEventListener("online", function (event) {
+            storage.storage.get(key, function ($news) {
+                if ($news) {
+                    all_news = $news;
+                    all_news.forEach(function (news) {
+                        showNewsOnPage(news.imgSrc, news.titleText, news.bodyText);
+                    });
+                }
+                storage.storage.delete(key);
+                sendToServer();
+                all_news = [];
+            });
+    });
 
-window.addEventListener("online",
-    function () {
-        if (isOnline() && localStorage.length > 0) {
-            showNewsFromLocalStorage();
-            sendToServer();
+    storage.storage.get("news", (news) => {
+        if (news) {
+            all_news = news;
         }
     });
 
-if (isOnline() && localStorage.length > 0) {
-    showNewsFromLocalStorage();
-}
-
-function isOnline() {
-    return window.navigator.onLine;
-}
-
-function showNewsFromLocalStorage() {
-    const $news = JSON.parse(localStorage.getItem(key));
-    localStorage.removeItem(key);
-    if ($news != null) {
-        $news.forEach(function (news) {
-            if (news != null) {
-                showNewsOnPage(news.imgSrc, news.titleText, news.bodyText);
+    if (isOnline()) {
+        // all_news.forEach(function (news) {
+        //     showNewsOnPage(news.imgSrc, news.titleText, news.bodyText);
+        // });
+        // storage.storage.delete(key);
+        // sendToServer();
+        // all_news = [];
+        storage.storage.get(key, function ($news) {
+            if ($news) {
+                all_news = $news;
+                all_news.forEach(function (news) {
+                    showNewsOnPage(news.imgSrc, news.titleText, news.bodyText);
+                });
             }
+            storage.storage.delete(key);
+            sendToServer();
+            all_news = [];
         });
     }
-}
+
+    function showNewsOnPage(imgSrc, titleText, bodyText) {
+        var newsID = document.getElementById('news');
+        const column = document.createElement("div");
+        const cardBlock = document.createElement("div");
+        var image = document.createElement("img");
+        const news = document.createElement("article");
+        const title = document.createElement("h6");
+        image.src = imgSrc;
+        image.alt = "Album";
+        image.id = "news-image";
+        title.innerHTML = titleText;
+        news.innerHTML = "<p>" + bodyText + "</p>";
+        cardBlock.appendChild(image);
+        cardBlock.appendChild(title);
+        cardBlock.appendChild(news);
+        column.appendChild(cardBlock);
+        newsID.appendChild(column);
+        cardBlock.className = "card";
+        column.className = "col-sm-12 col-md-6 col-lg-4";
+    }
 
 
-function showNewsOnPage(imgSrc, titleText, bodyText) {
-
-    // const imgSrc = "../images/Beatles_-_Abbey_Road.jpg";
-    // const titleText = "TEST SRUFHRFHR";
-    // const bodyText = "FVREF VERFsvdwfk vmerfvn ejvnrej vnrkjefvnrkjv  re rejvrkj rj rjk  r ";
-
-    var newsID = document.getElementById('news');
-    const column = document.createElement("div");
-    const cardBlock = document.createElement("div");
-    var image = document.createElement("img");
-    const news = document.createElement("article");
-    const title = document.createElement("h6");
-    image.src = imgSrc;
-    image.alt = "Album";
-    image.id = "news-image";
-    title.innerHTML = titleText;
-    news.innerHTML = "<p>" + bodyText + "</p>";
-    cardBlock.appendChild(image);
-    cardBlock.appendChild(title);
-    cardBlock.appendChild(news);
-    column.appendChild(cardBlock);
-    newsID.appendChild(column);
-    cardBlock.className = "card";
-    column.className = "col-sm-12 col-md-6 col-lg-4";
-}
-
-
-function sendToServer() {
-    alert("Sent to server");
-}
+});
